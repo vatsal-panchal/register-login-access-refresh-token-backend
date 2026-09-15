e132fda9e021ed616b9d63b87f6d9565aa2c9a56
vatsal-panchal <panchalvatsal2025@gmail.com>
2026-09-15 09:02:05 +0530

    done

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+  MONGO_URI: process.env.MONGO_URI,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  PORT: process.env.PORT || 3000,
+};
+
+export default config;
---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import config from "./config.js";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.MONGO_URI);
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.log(`DB connection fail || ${error.message}`);
+  }
+};
+
+
+export default connectDB
