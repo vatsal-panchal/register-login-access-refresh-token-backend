b228082e3c24f6fb51755609df433272425eaa15
vatsal-panchal <panchalvatsal2025@gmail.com>
2026-09-15 09:19:57 +0530

    code

---
+++ src/models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      lowercase: true,
+      minLength: [2, "Name must be at least 2 characters"],
+      maxLength: [30, "Name cannot exceed 30 characters"],
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      lowercase: true,
+      trim: true,
+      unique: true,
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        "Please enter a valid email address",
+      ],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [6, "Password must be at least 6 characters"],
+      maxLength: [72, "Password cannot exceed 72 characters"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+export const userModel = mongoose.model("user",userSchema)
